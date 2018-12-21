ac5b81ae4f6c18cee1ace72a81483b8546af169a
Matthew Kennedy <burnsidemk@gmail.com>
2018-12-21 09:47:30 -0600

    add remaining image function

--- im/im.lisp
+++ im/im.lisp
@@ -14,14 +14,41 @@
 
 (in-package #:cd-im)
 
+(export '(context-im-image
+          put-im-image
+          pattern-im-image
+          stipple-im-image))
+
 (defalias context-im-image #'cd-im-cffi::%cd-context-im-image)
-(defalias put-im-image #'cd-im-cffi::%cdf-canvas-put-im-image)
-(defalias put-im-image #'cd-im-cffi::%cdf-canvas-put-im-image)
 
+(defun put-im-image (canvas im-image x y w h)
+  "Draws an imImage on any canvas. Image must be a displayable
+image (imImageIsBitmap), and it can has an alpha channel."
+  (cd-im-cffi::%cdf-canvas-put-im-image
+   canvas im-image (coerce x 'double-float)
+   (coerce y 'double-float)
+   (coerce w 'double-float)
+   (coerce h 'double-float)))
 
-(cffi:defcfun ("%cd-canvas-put-im-image" "cdCanvasPutImImage") :void
-  (canvas ))
-void cdCanvasPutImImage(cdCanvas* canvas, const imImage* image, int x, int y, int w, int h); [in C]
-void cdfCanvasPutImImage(cdCanvas* canvas, const imImage* image, double x, double y, double w, double h); [in C]
-void wdCanvasPutImImage(cdCanvas* canvas, const imImage* image, double x, double y, double w, double h); (WC) [in C]
+(defun pattern-im-image (canvas im-image)
+  "Defines an imImage as a pattern on any canvas. Image must be a displayable
+image (imImageIsBitmap)."
+  (cd-im-cffi::%cd-canvas-stipple-im-image canvas im-image))
 
+(defun stipple-im-image (canvas im-image)
+  "Defines an imImage as a stipple on any canvas. Image must be a binary
+image (color_space=IM_BINARY)."
+  (cd-im-cffi::%cd-canvas-stipple-im-image canvas im-image))
+
+(in-package #:wd-im)
+
+(export '(put-im-image))
+
+(defun put-im-image (canvas im-image x y w h)
+  "Draws an imImage on any canvas. Image must be a displayable
+image (imImageIsBitmap), and it can has an alpha channel."
+  (cd-im-cffi::%wd-canvas-put-im-image
+   canvas im-image (coerce x 'double-float)
+   (coerce y 'double-float)
+   (coerce w 'double-float)
+   (coerce h 'double-float)))
